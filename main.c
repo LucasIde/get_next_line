a58d7bf6a0889e8a3142e05a06de685fafc3cf77
Lucas Ide <lide@fu-r10-p6.s19.be>
2022-02-11 19:51:10 +0100

    push rapide

--- main.c
+++ main.c
@@ -4,20 +4,42 @@
 
 int main(void)
 {
-	// write(1, "PAR ICI\n", 8);
-    // printf("salut");
-    char    *str;
-    int    fd;
+	char	*str;
+	int		fd;
+	// int		fd2;
 
-    fd = open("text.txt", O_RDONLY);
-    // fd = 0;
-    while ((str = get_next_line(fd)))
-    {
-        printf("%s", str);
-        // free(str);
-    }
-    close(fd);
-    return (0);
+	fd = open("text.txt", O_RDONLY);
+	// fd2 = open("text2.txt", O_RDONLY);
+	// fd = 0;
+	while ((str = get_next_line(fd)))
+	{
+		printf("%s", str);
+		free(str);
+	}
+	// str = get_next_line(fd);
+	// printf("1 - |%s|\n", str);
+	// free(str);
+	// str = get_next_line(fd);
+	// printf("2 - |%s|\n", str);
+	// free(str);
+	// str = get_next_line(fd2);
+	// printf("3 - |%s|\n", str);
+	// free(str);
+	// str = get_next_line(fd);
+	// printf("4 - |%s|\n", str);
+	// free(str);
+	// str = get_next_line(fd);
+	// printf("5 - |%s|\n", str);
+	// free(str);
+	// str = get_next_line(fd2);
+	// printf("6 - |%s|\n", str);
+	// free(str);
+	// str = get_next_line(fd2);
+	// printf("7 - |%s|\n", str);
+	// free(str);
+	close(fd);
+	// close(fd2);
+	return (0);
 }
 
 		// write(1, str, ft_strlen(str));
