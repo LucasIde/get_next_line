070f6e52d24c601538c362cb2fab023741b2de33
Lucas Ide <lide@fu-r7-p5.s19.be>
2022-03-25 14:07:12 +0100

    petit leaks

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: lide <lide@student.s19.be>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/04 12:08:24 by lide              #+#    #+#             */
-/*   Updated: 2022/03/02 16:53:02 by lide             ###   ########.fr       */
+/*   Updated: 2022/03/25 14:04:53 by lide             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,6 +41,14 @@ char	*ft_strjoin(char *s1, char *s2, int i, int j)
 	return (s3);
 }
 
+char	*free_error(char *str, char *save)
+{
+	if (save)
+		free(save);
+	free(str);
+	return (NULL);
+}
+
 char	*ft_read(int fd, char *save)
 {
 	char	*str;
@@ -48,23 +56,21 @@ char	*ft_read(int fd, char *save)
 
 	str = malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (!str)
+	{
+		if (save)
+			free(save);
 		return (NULL);
+	}
 	i = 1;
 	while (backspace(save) && i)
 	{
 		i = read(fd, str, BUFFER_SIZE);
 		if (i == -1 || (!save && i == 0))
-		{
-			free(str);
-			return (NULL);
-		}
+			return (free_error(str, save));
 		str[i] = 0;
 		save = ft_strjoin(save, str, -1, -1);
 		if (!save)
-		{
-			free(str);
-			return (NULL);
-		}
+			return (free_error(str, save));
 	}
 	free(str);
 	return (save);
@@ -88,7 +94,7 @@ char	*get_next_line(int fd)
 	save = ft_save(save, len);
 	if (!save)
 	{
-		free(line)
+		free(line);
 		return (NULL);
 	}
 	return (line);
